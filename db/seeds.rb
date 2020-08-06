18bd312ccda22a1c594271ca82e76f4510889946
jamescodegaard <jamescodegaard@gmail.com>
2020-08-06 11:34:17 -0500

    updates to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,17 @@
 #
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# function myFunction() {
+  
+# }
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   Character.create(name: 'Luke', movie: movies.first)
+
 businesses = Business.create([
   {
     name: "Oneota Co-Op",
@@ -16,7 +27,8 @@ businesses = Business.create([
     website: "https://oneotacoop.com/",
     description: "The mission of the Oneota Community Food Co-op is to build vibrant communities and ecosystems by providing organic, locally produced and bulk foods, as well as other products and services that are sustainable for those who consume and produce them.",
     hours: "Monday: 9AM–7PM, Tuesday: 9AM–7PM, Wednesday: 9AM–7PM, Thursday: 9AM–7PM, Friday: 9AM–7PM, Saturday: 9AM–7PM, Sunday: 9AM–7PM",
-    image: "https://www.welcometothetable.coop/sites/default/files/logo-oneota-community-co-op.jpg"
+    image: "/img/decorah/oneotacoop.jpg",
+    category: "Other"
   },
   {
     name: "Pulpit Rock Brewing",
@@ -28,7 +40,8 @@ businesses = Business.create([
     website: "https://www.pulpitrockbrewing.net/",
     description: "Pulpit Rock Brewing opened its bay doors during the summer of ‘15. Founded by four Luther Alumni, fueled by two creative head-brewers and driven by a small taproom team, this modest brewery embraces the opportunity to produce and serve exciting and innovative beers.",
     hours: "Monday-Thursday: 2PM-8PM, Monday-Thursday: 2PM-8PM",
-    image: "https://cdn.beeradvocate.com/im/places/42001.jpg"
+    image: "/img/decorah/pulpitrock.jpg",
+    category: "Brewery"
   },
   {
     name: "Impact Coffee",
@@ -39,19 +52,33 @@ businesses = Business.create([
     phone: "(563) 419-3141",
     website: "https://www.impactcoffee.com/",
     description: "We are a specialty coffee bar and roastery located in Decorah Iowa. Our bar features single origin pour-overs, creatively crafted espresso drinks, and nitro cold brew on draft!",
-    hours: "Monday: 8AM - 3PM, Tuesday: 8AM - 3PM, Wednesday: 8AM - 3PM, Thursday: 8AM - 7PM, Friday: 8AM - 7PM,Saturday: 8AM - 6PM, Sunday: 8AM - 1PM",
-    image: "https://cdn.shopify.com/s/files/1/2596/7938/files/Impact_Logo_Final_White.png_2_400x200.png?v=1586899776"
+    hours: "Monday-Friday: 6AM - 6PM,Saturday: 6AM - 8PM, Sunday: 8AM - 1PM",
+    image: "/img/decorah/impact.jpg",
+    category: "Coffee Shop"
+  },
+  {
+    name: "Doug Boodle Studios",
+    email: "boodle@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    address: "501 High St, Decorah, IA 52101",
+    phone: "(563) 380-9558",
+    website: "https://www.facebook.com/Doug-Boodle-199059877397514",
+    description: "Ready to noodle & boodle to fun local bands? We're the spot for you. We promote local artists and help them to collaborate within Decorah, and beyond. Swing by for a show or just to say hey - all are welcome!",
+    hours: "Wednesday-Saturday: 12PM - 12AM",
+    image: "/img/decorah/dougboodle.jpg",
+    category: "Music Venue"
   }
 ])
 
 events = Event.create([
   {
     name: "Saftig Summer Jam",
-    date: "2020-08-28 17:00:00",
+    date: "2020-08-28 15:00:00",
     description: "Come join us for a fun night of music and an outdoor beer extravaganza. We'll being pouring until the kegs are tapped - enjoy the [properly socially-distanced] fun in the sun!",
     alt_contact: "Brewmaster Bob",
     alt_email: "bob@gmail.com",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ80TTxXrHzd5kNuXTJQQDDoR-WnSjspXffdA&usqp=CAU",
+    image: "/img/decorah/events/pulpit-beer-release.jpg",
     business_id: 2
   },
   {
@@ -60,8 +87,53 @@ events = Event.create([
     description: "The name says it all...have a blast with your favorite local drag queens while sipping on your favorite local coffee/cocktail/beverage of choice! No reservation necessary - and don't remember to bring your tips!",
     alt_contact: "Gabe",
     alt_email: "gabe@gmail.com",
-    image: "https://media-cdn.tripadvisor.com/media/photo-s/19/d6/b2/fe/sitting-areas.jpg",
+    image: "/img/decorah/events/impact-drag.jpg",
     business_id: 3
+  },
+  {
+    name: "Concert Series: Local Feature Week 1",
+    date: "2020-09-03 20:00:00",
+    description: "Throughout the Fall we'll be featuring local bands in a weekly concert series. The official lineup for each concert will be announced the week prior. Admission is free and we hope you'll come to support our artists.",
+    alt_contact: "Seamus",
+    alt_email: "seamus@gmail.com",
+    image: "/img/decorah/events/boodle-concertseries.jpg",
+    business_id: 4
+  },
+  {
+    name: "Concert Series: Local Feature Week 2",
+    date: "2020-09-10 20:00:00",
+    description: "Throughout the Fall we'll be featuring local bands in a weekly concert series. The official lineup for each concert will be announced the week prior. Admission is free and we hope you'll come to support our artists.",
+    alt_contact: "Seamus",
+    alt_email: "seamus@gmail.com",
+    image: "/img/decorah/events/boodle-concertseries.jpg",
+    business_id: 4
+  },
+  {
+    name: "Concert Series: Local Feature Week 3",
+    date: "2020-09-17 20:00:00",
+    description: "Throughout the Fall we'll be featuring local bands in a weekly concert series. The official lineup for each concert will be announced the week prior. Admission is free and we hope you'll come to support our artists.",
+    alt_contact: "Seamus",
+    alt_email: "seamus@gmail.com",
+    image: "/img/decorah/events/boodle-concertseries.jpg",
+    business_id: 4
+  },
+  {
+    name: "Concert Series: Local Feature Week 4",
+    date: "2020-09-24 20:00:00",
+    description: "Throughout the Fall we'll be featuring local bands in a weekly concert series. The official lineup for each concert will be announced the week prior. Admission is free and we hope you'll come to support our artists.",
+    alt_contact: "Seamus",
+    alt_email: "seamus@gmail.com",
+    image: "/img/decorah/events/boodle-concertseries.jpg",
+    business_id: 4
+  },
+  {
+    name: "Concert Series: Local Feature Week 5",
+    date: "2020-10-01 20:00:00",
+    description: "Throughout the Fall we'll be featuring local bands in a weekly concert series. The official lineup for each concert will be announced the week prior. Admission is free and we hope you'll come to support our artists.",
+    alt_contact: "Seamus",
+    alt_email: "seamus@gmail.com",
+    image: "/img/decorah/events/boodle-concertseries.jpg",
+    business_id: 4
   }
 ])
 
@@ -90,5 +162,46 @@ event_tags = EventTag.create([
   {
     event_id: 2,
     tag_id: 6
+  },
+  {
+    event_id: 3,
+    tag_id: 1
+  },
+  {
+    event_id: 3,
+    tag_id: 6
+  },
+  {
+    event_id: 4,
+    tag_id: 1
+  },
+  {
+    event_id: 4,
+    tag_id: 6
+  },
+  {
+    event_id: 5,
+    tag_id: 1
+  },
+  {
+    event_id: 5,
+    tag_id: 6
+  },
+  {
+    event_id: 6,
+    tag_id: 1
+  },
+  {
+    event_id: 6,
+    tag_id: 6
+  },
+  {
+    event_id: 7,
+    tag_id: 1
+  },
+  {
+    event_id: 7,
+    tag_id: 6
   }
+  
 ])
